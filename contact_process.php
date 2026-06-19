0a9c507dea30a8b99067da4ecd64056b9a2e462b
Max-123-ux <sboy62667@gmail.com>
2026-06-18 19:20:12 -0800

    Add files via upload

---
+++ contact_process.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'db.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contact_messages (name, email, message)
+VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+
+?>
